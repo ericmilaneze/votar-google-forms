57fda50531c628b1e46e451fa5e51478bad61475
Eric Milaneze <eric@milaneze.com.br>
2020-07-01 22:49:47 -0300

    Humanizando.

--- src/Votar/Program.cs
+++ src/Votar/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.IO;
 using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
 
 namespace Votar
 {
@@ -17,57 +20,100 @@ namespace Votar
         static void Main(string[] args)
         {
             int voteCount = 0;
+            int errorCount = 0;
 
-            //while (true)
-            //{
-                using (var driver = GetDriver(voteCount))
+            while (true)
+            {
+                try
                 {
-                    try
-                    {
+                    using (var driver = GetDriver(voteCount, errorCount))
                         VoteInPage(driver);
-                        voteCount++;
-                    }
-                    catch (Exception ex)
-                    {
-                        string logFilePath = Path.Combine(LOGS_PATH, "votar-{DateTime.Now:yyyyMMddHHmmssfff}");
-                        using (StreamWriter writer = new StreamWriter(logFilePath))
-                            writer.WriteLine(ex.Message);
-                    }
+                    voteCount++;
+                }
+                catch (Exception ex)
+                {
+                    LogError(ex);
+                    errorCount++;
                 }
 
                 Console.Clear();
                 Console.WriteLine($"Contagem: {voteCount}");
-                Thread.Sleep(2);
-            //}   
+                Task.Delay(4000).Wait();
+            }
+        }
+
+        private static void LogError(Exception ex)
+        {
+            try
+            {
+                string logFilePath = Path.Combine(LOGS_PATH, $"votar-{DateTime.Now:yyyyMMddHHmmssfff}");
+                using (StreamWriter writer = new StreamWriter(logFilePath, true))
+                    writer.WriteLine(ex.Message);
+            }
+            catch { }
         }
 
         private static void VoteInPage(RemoteWebDriver driver)
         {
             driver.Navigate().GoToUrl(URL);
+
+            Task.Delay(1000).Wait();
             var radioButton = driver.FindElementByCssSelector("div.appsMaterialWizToggleRadiogroupEl.exportToggleEl[data-value~=Suelen]");
             radioButton.Click();
+
+            Task.Delay(1000).Wait();
             var submitButton = driver.FindElementByCssSelector("div.freebirdFormviewerViewNavigationSubmitButton[role=button]");
             submitButton.Click();
+
+            WaitForResponse(driver);
+            Task.Delay(1000).Wait();
         }
 
-        private static RemoteWebDriver GetDriver(int voteCount)
+        private static void WaitForResponse(RemoteWebDriver driver)
         {
-            if (voteCount % 2 == 0)
+            try
             {
-                // var options = new ChromeOptions();
-                // options.AddArgument("--silent");
-                // options.AddArgument("--disable-gpu");
-                // options.AddArgument("--log-level=3");
-
-                var service = ChromeDriverService.CreateDefaultService(DRIVERS_DIRECTORY);
-                service.HideCommandPromptWindow = true;
-                service.SuppressInitialDiagnosticInformation = true;
-
-                //return new ChromeDriver(service, options);
-                return new ChromeDriver(service);
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
+                wait.Until(d => d.FindElement(By.CssSelector(".freebirdFormviewerViewResponseLinksContainer a")));
             }
+            catch (Exception ex)
+            {
+                LogError(ex);
+            }
+        }
 
-            return new FirefoxDriver(DRIVERS_DIRECTORY);
+        private static RemoteWebDriver GetDriver(int voteCount, int errorCount)
+        {
+            bool isEven = (voteCount + errorCount) % 2 == 0;
+            if (isEven)
+                return GetFirefoxDriver();
+            return GetChromeDriver();
+        }
+
+        private static RemoteWebDriver GetFirefoxDriver()
+        {
+            var service = FirefoxDriverService.CreateDefaultService(DRIVERS_DIRECTORY);
+            service.HideCommandPromptWindow = true;
+            service.SuppressInitialDiagnosticInformation = true;
+
+            var options = new FirefoxOptions();
+            options.LogLevel = FirefoxDriverLogLevel.Error;
+
+            return new FirefoxDriver(service, options);
+        }
+
+        private static RemoteWebDriver GetChromeDriver()
+        {
+            var service = ChromeDriverService.CreateDefaultService(DRIVERS_DIRECTORY);
+            service.HideCommandPromptWindow = true;
+            service.SuppressInitialDiagnosticInformation = true;
+
+            var options = new ChromeOptions();
+            options.AddArgument("--silent");
+            options.AddArgument("--disable-gpu");
+            options.AddArgument("--log-level=3");
+
+            return new ChromeDriver(service);
         }
     }
 }
